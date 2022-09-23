c5e6c197ab3c0b3541aa3c4a323294f26445b493
ignacioborraz <ignacioborraz@hotmail.com>
2022-09-23 18:10:01 -0300

    all states

--- router/plate.js
+++ router/plate.js
@@ -1,24 +1,21 @@
-const Router = require('express').Router()
+const router = require('express').Router()
 //const validator = require('./config/validator')
 const passport = require('../config/passport')
 
 const {
-    createPlate,getPlates,getLastStateOfPlates,getOnePlate,putPlate,changeState,deletePlate
+    createPlate,getPlates,getOnePlate,putPlate,changeState,deletePlate
 } = require('../controllers/plateControllers')
 
-Router.route('/')
+router.route('/')
     .get(passport.authenticate('jwt', {session:false}),getPlates)
     .post(passport.authenticate('jwt', {session:false}),createPlate)
 
-Router.route('/lasts')
-    .get(passport.authenticate('jwt', {session:false}),getLastStateOfPlates)
-
-Router.route('/:id')
+router.route('/:id')
     .get(passport.authenticate('jwt', {session:false}),getOnePlate)
     .put(passport.authenticate('jwt', {session:false}),putPlate)
     .delete(passport.authenticate('jwt', {session:false}),deletePlate)
 
-Router.route('/change/:id')
+router.route('/change/:id')
     .post(passport.authenticate('jwt', {session:false}),changeState)
 
-module.exports = Router
+module.exports = router
