4919b3271fdf178b9f0a90aa365f298eed7f0052
ignacioborraz <ignacioborraz@hotmail.com>
2022-09-25 00:32:22 -0300

    marmol  granito

---
+++ config/data/granito.js
@@ -0,0 +1,139 @@
+require('dotenv').config()
+require('../database')
+const Color = require('../../models/Color')
+
+let colors = [
+    {
+        name: "Amarillo Brasil",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Beige Puma",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Blanco Ceara",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Blanco Dallas",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Blanco París y/o Fortaleza",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Coffee Brown",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Ebony Crystal",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Galaxy Black",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Gris Mara",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Gris Perla",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Marrón Coco",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Napolito",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Negro Absoluto Extra",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Negro Boreal",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Negro Brasil",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Negro Brasil Leather",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Negro Indiano",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Rojo Dragón",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Rojo Príncipe",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Rosa Del Salto",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Sierra Chica Marrón",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Sierra Chica Rojo",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Titanium",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Verde San Francisco",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    },
+    {
+        name: "Verde Ubatub",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fgranito.jpg?alt=media&token=be1f3e89-358c-4419-b848-5fcc7adca6e1",
+        company: "62e5ff9be3ef843cb2fd1a4c"
+    }
+]
+
+colors.forEach(color => {
+    Color.create({
+        name: color.name,
+        photo: color.photo,
+        company: color.company
+    })
+})
---
+++ config/data/marmol.js
@@ -0,0 +1,65 @@
+require('dotenv').config()
+require('../database')
+const Color = require('../../models/Color')
+
+let colors = [
+    {
+        name: "Beige Arena",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fmarmol.jpg?alt=media&token=cea1a412-6e98-484e-b500-8e3346a75ac9",
+        company: "62e5ff27e3ef843cb2fd1a4a"
+    },
+    {
+        name: "Beige Sahara",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fmarmol.jpg?alt=media&token=cea1a412-6e98-484e-b500-8e3346a75ac9",
+        company: "62e5ff27e3ef843cb2fd1a4a"
+    },
+    {
+        name: "Blanco Carrara",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fmarmol.jpg?alt=media&token=cea1a412-6e98-484e-b500-8e3346a75ac9",
+        company: "62e5ff27e3ef843cb2fd1a4a"
+    },
+    {
+        name: "Blanco Neve",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fmarmol.jpg?alt=media&token=cea1a412-6e98-484e-b500-8e3346a75ac9",
+        company: "62e5ff27e3ef843cb2fd1a4a"
+    },
+    {
+        name: "Blanco Turco",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fmarmol.jpg?alt=media&token=cea1a412-6e98-484e-b500-8e3346a75ac9",
+        company: "62e5ff27e3ef843cb2fd1a4a"
+    },
+    {
+        name: "Botticino",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fmarmol.jpg?alt=media&token=cea1a412-6e98-484e-b500-8e3346a75ac9",
+        company: "62e5ff27e3ef843cb2fd1a4a"
+    },
+    {
+        name: "Calacatta Milano",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fmarmol.jpg?alt=media&token=cea1a412-6e98-484e-b500-8e3346a75ac9",
+        company: "62e5ff27e3ef843cb2fd1a4a"
+    },
+    {
+        name: "Marrón Bosque",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fmarmol.jpg?alt=media&token=cea1a412-6e98-484e-b500-8e3346a75ac9",
+        company: "62e5ff27e3ef843cb2fd1a4a"
+    },
+    {
+        name: "Marrón Emperador",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fmarmol.jpg?alt=media&token=cea1a412-6e98-484e-b500-8e3346a75ac9",
+        company: "62e5ff27e3ef843cb2fd1a4a"
+    },
+    {
+        name: "Travertino",
+        photo: "https://firebasestorage.googleapis.com/v0/b/portaro-edc21.appspot.com/o/placas%2Fmarmol.jpg?alt=media&token=cea1a412-6e98-484e-b500-8e3346a75ac9",
+        company: "62e5ff27e3ef843cb2fd1a4a"
+    },
+
+]
+
+colors.forEach(color => {
+    Color.create({
+        name: color.name,
+        photo: color.photo,
+        company: color.company
+    })
+})
